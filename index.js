6c9ce6d03753e19aa7259096fad7eb47d0d8c662
salonidhatrak <salonidhatrak123@gmail.com>
2023-04-05 10:45:21 +0530

    css

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-console.log("hello")
-console.log("saloni")
+// function add(item,list){
+//     return list.push(item)
+// }
+
+// const result=add("appple",["banana","orange"]);
+// console.log(result);
+
+// for(var i=0; i<3; i++){
+//     setTimeout(()=>console.log(i),1)
+// }
+
+// (()=>{
+//     let x=(y=10);
+// })()
+
+// console.log(typeof x);
+// console.log(typeof y);
+
+// const Person={
+//     name:"Ladiya",
+//     age:21,
+    
+// }
+
+// const admin={admin:true,...Person}
+// console.log(admin);
+
+
+const arr=[1,2,3];
+newArr=arr.filter(el=>el>1)
+console.log(newArr)
